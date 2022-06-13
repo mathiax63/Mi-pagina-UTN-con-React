6cc1e1141169d0d8cd864690261397a19a7c80c8
mathiax63 <mathyoyo@hotmail.es>
2022-06-13 20:33:30 -0300

    subo inicios

---
+++ src/components/layout/Nav.js
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom"
+const Nav = (props) => {
+    return (
+        <nav>
+            <div className="holder">
+                <ul>
+                    <li><Link to="/">Inicio</Link></li>
+                    <li><Link to="/paginasParaVer">Plataformas para ver sus peliculas</Link></li>
+                    <li><Link to="/todasLasPeliculas">Todos los productos</Link></li>
+                    <li><Link to="/admin">Soy administrador</Link></li>
+                    <li><Link to="/detalle">Por ahora</Link></li>
+                </ul>
+            </div>
+        </nav>
+       
+    )
+}
+export default Nav
